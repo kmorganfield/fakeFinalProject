6028064e82eacc4ea107c8832b06cf6fb396bc08
kmorganfield <kennedymorganfield@gmail.com>
2020-07-14 13:36:20 -0500

    Initial Commit

---
+++ fakeFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fakeFinalProject
+//
+//  Created by Kennedy Morganfield on 7/14/20.
+//  Copyright © 2020 Kennedy Morganfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
